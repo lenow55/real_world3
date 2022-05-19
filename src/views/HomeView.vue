492399d39e58c5748491b40f87a99dfb4dce2fbc
Новиков Илья <nov.ilucha@yandex.ru>
2022-05-19 20:49:41 +0300

    лекция 3 компоненты

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Events for Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 5928101,
+          category: "animal welfale",
+          title: "Cat Adoom Day",
+          location: "Meow Town",
+          date: "January 28, 2002",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "Cat Laydee",
+        },
+        {
+          id: 4587456,
+          category: "animal sdf",
+          title: "Cat Adoom Dasdfy",
+          location: "Meow Town",
+          date: "January 278, 2d002",
+          time: "19:00",
+          petsAllowed: true,
+          organizer: "Cat sdfds",
+        },
+        {
+          id: 3789458,
+          category: "aneme sdf",
+          title: "Cat Eva Dasdfy",
+          location: "Neco Town",
+          date: "January 278, 2d002",
+          time: "19:00",
+          petsAllowed: true,
+          organizer: "Neko sdfds",
+        },
+      ],
+    };
   },
 };
 </script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
