01c94251808d03d3d607cee863474946fcbf2c16
Новиков Илья <nov.ilucha@yandex.ru>
2022-05-21 13:05:33 +0300

    добавил анимацию запроса

--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -1,31 +1,83 @@
 <template>
-  <div>
+  <div v-if="event">
     <h1>{{ event.title }}</h1>
     <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
     <p>{{ event.description }}</p>
   </div>
+  <svg v-else class="spinner" viewBox="0 0 50 50">
+    <circle
+      class="path"
+      cx="25"
+      cy="25"
+      r="20"
+      fill="none"
+      stroke-width="5"
+    ></circle>
+  </svg>
 </template>
 
 <script>
 import EventService from "@/services/EventService";
 export default {
+  props: ["id"],
   data() {
     return {
       event: null,
-      id: 5928101,
     };
   },
   created() {
-    EventService.getEvent(this.id)
-      .then((response) => {
-        console.log("events", response.data);
-        this.event = response.data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    setTimeout(
+      async function () {
+        EventService.getEvent(this.id)
+          .then((response) => {
+            console.log("events", response.data);
+            this.event = response.data;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }.bind(this),
+      1000
+    );
   },
 };
 </script>
 
-<style></style>
+<style>
+.spinner {
+  animation: rotate 2s linear infinite;
+  z-index: 2;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin: -25px 0 0 -25px;
+  width: 50px;
+  height: 50px;
+}
+.path {
+  stroke: rgb(53, 117, 181);
+  stroke-linecap: round;
+  animation: dash 1.5s ease-in-out infinite;
+}
+
+@keyframes rotate {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes dash {
+  0% {
+    stroke-dasharray: 1, 150;
+    stroke-dashoffset: 0;
+  }
+  50% {
+    stroke-dasharray: 90, 150;
+    stroke-dashoffset: -35;
+  }
+  100% {
+    stroke-dasharray: 90, 150;
+    stroke-dashoffset: -124;
+  }
+}
+</style>
